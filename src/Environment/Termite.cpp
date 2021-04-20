a4443087aa11b2295f02570455983d611f79b540
Guillaume Belissent <guillaumebelissent@Guillaumes-MacBook-Pro.local>
2021-04-20 12:44:16 +0200

    Staged and added

---
+++ src/Environment/Termite.cpp
@@ -0,0 +1,40 @@
+#include "Termite.hpp"
+#include "../Application.hpp"
+#include "../Utility/Utility.hpp"
+#include "Environment.hpp"
+
+Termite::Termite()
+
+{
+
+}
+
+Termite::Termite(const ToricPosition& pos)
+    :Animal::Animal(pos, getAppConfig().termite_hp, getAppConfig().termite_lifespan)
+{
+    //Done
+}
+
+Termite::Termite(const Vec2d& pos)
+    :Termite(ToricPosition(pos))
+{
+    //Done
+}
+
+double Termite::getSpeed() const
+{
+    return getAppConfig().termite_speed;
+}
+
+sf::Sprite Termite::getSprite() const
+{
+    return buildSprite((getPosition()).toVec2d(),
+                       DEFAULT_TERMITE_SIZE,
+                       getAppTexture(getAppConfig().termite_texture),
+                       getDirection()/DEG_TO_RAD);
+}
+
+bool Termite::isEnemy(Animal const* animal)
+{
+    return !isDead() && !animal->isDead() && animal->isEnemyDispatch(this);
+}
